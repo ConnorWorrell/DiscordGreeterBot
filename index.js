946c6559afbf080d20aabb8e5d7e69f9d4463631
ConnorWorrell <chemicalcoal1@gmail.com>
2018-10-23 20:19:21 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () =>{
+
+	console.log(`${bot.user.username} is online!`)
+	bot.user.setActivity("with BOTy stuff");
+
+})
+
+bot.on("message", async message =>{
+
+	console.log(`${message.createdAt} ${message.author} ${message.channel} ${message.content}`)
+
+	if(message.author.bot) return;
+	if(message.channel.type === "dm") return;
+
+	let prefix = botconfig.prefix;
+	let messageArray = message.content.split(" ");
+	let cmd = messageArray[0];
+	let args = messageArray.slice(1);
+
+	if(cmd === `${prefix}hello`){
+		return message.channel.send("Hello!");
+	}
+
+	if(cmd === `${prefix}serverinfo`){
+
+	let sicon = message.guild.iconURL;
+	let serverembed = new Discord.RichEmbed()
+	.setDescription("Server Information")
+	.setColor("#15f153")
+	.setThumbnail(sicon)
+	.addField("Server Name", message.guild.name)
+	.addField("Created On", message.guild.createdAt)
+	.addField("You Joined", message.member.joinedAt)
+	.addField("Total Members", message.guild.memberCount);
+
+	return message.channel.send(serverembed);
+
+	}
+
+	if(cmd === `${prefix}botinfo`){
+
+
+		let bicon = bot.user.displayAvatarURL;
+		let botembed = new Discord.RichEmbed()
+		.setDescription("Bot Information")
+		.setColor("#15f153")
+		.setThumbnail(bicon)
+		.addField("Bot Name", bot.user.username)
+		.addField("Created On", bot.user.createdAt);
+
+		return message.channel.send(botembed);
+	}
+
+	if(cmd === `${prefix}report`){
+
+		let rUser = message.guild.member(message.mentions.users.first()  || message.guild.members.get(args[0]));
+		if(!rUser) return message.channel.send("Couldn't find user.");
+		let reason = args.join(" ").slice(22);
+
+		let reportEmbed = new Discord.RichEmbed()
+		.setDescription("Reports")
+		.setColor("#15f153")
+		.addField("Reported User", `${rUser} with ID: ${rUser.id}`)
+		.addField("Reported By", `${message.author} with ID: ${message.author.id}`)
+		.addField("Channel", message.channel)
+		.addField("Time", message.createdAt)
+		.addField("Reason", reason);
+
+		let reportschannel = message.guild.channels.find(x => x.name === "reports");
+		if(!reportschannel) return message.channel.send("Couldn't find reports channel.")
+
+
+		message.delete().catch(O_o=>{});
+		reportschannel.send(reportEmbed);
+		return;
+
+	}
+
+	if(cmd === `${prefix}say`){
+		messageReturned = messageArray.slice(1).join(" ");
+		console.log(`Deleted ${message}, Responded ${messageReturned}`);
+		message.delete().catch(O_o=>{});
+		return message.channel.send(messageReturned);
+	}
+
+})
+
+bot.login(botconfig.token);
+
+
