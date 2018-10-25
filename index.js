a0da0359ffefe246c183b8ee7e98dac3d654e700
ConnorWorrell <chemicalcoal1@gmail.com>
2018-10-24 18:13:54 -0600

    Added yandex translate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
+const axios = require("axios")
 
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -24,11 +25,28 @@ bot.on("message", async message =>{
 	let cmd = messageArray[0];
 	let args = messageArray.slice(1);
 
+	//Tranlation of all text to english using yandex's api
+	//Powered by yandex
+	axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate',
+		{
+			params: {
+				key: botconfig.YANDEX_API_KEY,
+				text: message.content,
+				lang: 'en'
+			}
+		}).then(res => {
+			if (res.data.text[0] !== message.content){//check to see if translation english -> english
+				message.reply(res.data.text[0])
+			}
+		})
+
+	//Lets Play Ping Pong
 	if(cmd === `${prefix}ping`){
 		console.log(`Bot Pongs the ping`);
 		return message.channel.send("Pong");
 	}
 
+	//Display server info
 	if(cmd === `${prefix}serverinfo`){
 
 		console.log(`Bot Displayed Server Info`);
@@ -47,6 +65,7 @@ bot.on("message", async message =>{
 
 	}
 
+	//Display bot info
 	if(cmd === `${prefix}botinfo`){
 		console.log(`Bot Displayed Info`);
 		let bicon = bot.user.displayAvatarURL;
@@ -60,11 +79,12 @@ bot.on("message", async message =>{
 		return message.channel.send(botembed);
 	}
 
+	//Report Command
 	if(cmd === `${prefix}report`){
 		console.log(`User Reported`);
-		let rUser = message.guild.member(message.mentions.users.first()  || message.guild.members.get(args[0]));
-		if(!rUser) return message.channel.send("Couldn't find user.");
-		let reason = args.join(" ").slice(22);
+		let rUser = message.guild.member(message.mentions.users.first()  || message.guild.members.get(args[0]));//First user mentioned is user being reported
+		if(!rUser) return message.channel.send("Couldn't find user.");//Check to see if user was reported
+		let reason = args.join(" ").slice(22);//remove the first 22 characters (nameID) from message
 
 		let reportEmbed = new Discord.RichEmbed()
 		.setDescription("Reports")
@@ -75,29 +95,31 @@ bot.on("message", async message =>{
 		.addField("Time", message.createdAt)
 		.addField("Reason", reason);
 
-		let reportschannel = message.guild.channels.find(x => x.name === "reports");
-		if(!reportschannel) return message.channel.send("Couldn't find reports channel.")
+		let reportschannel = message.guild.channels.find(x => x.name === "reports");//Find reports channel
+		if(!reportschannel) return message.channel.send("Couldn't find reports channel.")//If you can't find reports channel
 
 
-		message.delete().catch(O_o=>{});
-		reportschannel.send(reportEmbed);
+		message.delete().catch(O_o=>{});//Catch reports message
+		reportschannel.send(reportEmbed);//Display imbed in reports channel
 		return;
 
 	}
 
+	//Make the bot say something
 	if(cmd === `${prefix}say`){
-		messageReturned = messageArray.slice(1).join(" ");
+		messageReturned = messageArray.slice(1).join(" ");//Remove "!say" from beginning of message
 		console.log(`Deleted ${message}, Responded ${messageReturned}`);
 		message.delete().catch(O_o=>{});
 		return message.channel.send(messageReturned);
 	}
 
+	//Delete last 100 messages
 	if(cmd === `${prefix}clean`){
 		console.log(`Cleaned Messages`);
 		async function clear() {
-			message.delete().catch(O_o=>{});
-			const fetched = await message.channel.fetchMessages({limit:99});
-			message.channel.bulkDelete(fetched);
+			message.delete().catch(O_o=>{});//Delete clean message
+			const fetched = await message.channel.fetchMessages({limit:99});//Find last 99 messages
+			message.channel.bulkDelete(fetched);//Delete the found messages
 		}
 		clear();
 	}
