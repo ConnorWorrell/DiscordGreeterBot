c09f1db825167d2e6a531e8d84a3caa7faaf9e36
ConnorWorrell <chemicalcoal1@gmail.com>
2018-10-27 17:36:35 -0600

    fixed bug where ban and kick command without a target would err

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	let bUser = message.guild.member(message.mentions.users.first()  || message.guild.members.get(args[0]));
-	if(!bUser) message.channel.send("Can't find user");
+	if(!bUser) return message.channel.send("Can't find user");
 	let bReason = args.join(" ").slice(22);
 	if(!message.member.permissions.has("BAN_MEMBERS")) return message.channel.send("Error: Impropper Permissions");
 	if(bUser.permissions.has("BAN_MEMBERS")) return message.channel.send("Error: Can't ban someone with equal or higher permisions to you");
