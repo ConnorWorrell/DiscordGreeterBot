050325f5aff827f0f542a1b2752329a685f2beef
ConnorWorrell <chemicalcoal1@gmail.com>
2018-10-27 16:18:40 -0600

    added kick command

--- index.js
+++ index.js
@@ -150,6 +150,30 @@ bot.on("message", async message =>{
 		return;
 	}
 
+	if(cmd.toLowerCase() === `${prefix}kick`){
+
+		let kUser = message.guild.member(message.mentions.users.first()  || message.guild.members.get(args[0]));
+		if(!kUser) message.channel.send("Can't find user");
+		let kReason = args.join("").slice(22);
+		if(!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send("Error: Impropper Permissions");
+		if(kUser.permissions.has("KICK_MEMBERS")) return message.channel.send("Error: Can't kick someone with equal or higher permisions to you");
+
+		let kickEmbed = new Discord.RichEmbed()
+		.setDescription("Kick")
+		.addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+		.addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+		.addField("Kicked In", message.channel)
+		.addField("Time", message.createdAt)
+		.addField("Reason", kReason);
+
+		let kickChannel = message.guild.channels.find(x => x.name === "incidents");
+		if(!kickChannel) return message.channel.send("Can't find incidents channel");
+
+		message.guild.member(kUser).kick(kReason);
+		kickChannel.send(kickEmbed);
+
+	}
+
 	//Tranlation of all text to english using yandex's api
 	//Powered by yandex
 	axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate',
